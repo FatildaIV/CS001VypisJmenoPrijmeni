d5eb7e81956622f81f9c4ddf5442210edd01106b
Fatilda IV <30930670+FatildaIV@users.noreply.github.com>
2021-06-11 11:06:20 +0200

    Add files via upload

---
+++ CS001VypisJmenoPrijmeni/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS001VypisJmenoPrijmeni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Karel Hofman");
+            Console.ReadKey();
+            System.Threading.Thread.Sleep(5000);
+            Console.Beep();
+            Console.Write("K");
+            Console.Beep();
+            Console.Write("a");
+            Console.Beep();
+            Console.Write("r");
+            Console.Beep();
+            Console.Write("e");
+            Console.Beep();
+            Console.Write("l");
+            Console.Beep();
+            Console.Write(" ");
+            Console.Write("H");
+            Console.Beep();
+            Console.Write("o");
+            Console.Beep();
+            Console.Write("f");
+            Console.Beep();
+            Console.Write("m");
+            Console.Beep();
+            Console.Write("a");
+            Console.Beep();
+            Console.Write("n");
+            Console.Beep();
+            Console.ReadKey();
+        }
+    }
+}
